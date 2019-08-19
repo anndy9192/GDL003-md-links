e7cfdf48576222e553ee76b6e76928a9c471adc6
anndy9192 <andybmq@gmail.com>
2019-08-19 18:40:54 -0500

    Se crea prier funcion para buscar archvos .md y su test correspondiente

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-module.exports = () => {
-  // ...
-};
+const path = require('path');
+//export permite que use la logica de otro archivo (asi node sabe que hay mas logica en otros archivos)
+module.exports = filePath => path.extname(filePath) === '.md';
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,26 @@
-const mdLinks = require('../');
+const mdLinks = require('../index.js');
 
 
 describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+//ponemos una breve descripcion de lo que queremos que haga esa funcion
+  it('should retun tru for a valid .md file', () => {
+    //expect es la expectativa mdLinks el nombre de mi funcion y dentro de los parentesis la ruta para ver si sirve esa funcion  y el .tobe para ver lo que esperamos 
+    expect(mdLinks('../README.md')).toBe(true);
+    
   });
+  it('should retun false for a valid .md file', () => {
+    expect(mdLinks('../index.js')).toBe(false);
+    
+});
+
+/*describe('searchLinks', () => {
+
+  it('should return the links found', () =>{
+    expect(searchLinks())
+  } )
+
+  
+});
+*/
 
 });
